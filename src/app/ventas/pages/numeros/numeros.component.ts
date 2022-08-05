a8da9302378de2107a62b5cfcdb3456c61be101a
Oscar Davila <oscar7822002@gmail.com>
2022-08-05 10:35:14 -0500

    DecimalPipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html'
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ventasNetas: number = 2567789.5567;
+  porcentaje: number = 0.48;  
 
 }
