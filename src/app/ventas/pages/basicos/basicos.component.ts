1d77be0266eb59ac54eb12cd4dbf69e90361f92b
Oscar Davila <oscar7822002@gmail.com>
2022-08-05 09:39:00 -0500

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nombreLower: string = 'oscar'
+  nombreUpper: string = 'OSCAR'
+  nombreCompleto: string = 'osCAr dávIlA'
 
 }
